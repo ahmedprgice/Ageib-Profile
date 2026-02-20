bc7ebfcbe448bf265d7b05a6860fb0c0f7a4acaf
ahmedageib <ahmedageibemail01@gmail.com>
2026-02-20 08:08:41 +0800

    loader added

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,7 @@ import { Experience } from "./components/Experience";
 import { Education } from "./components/Education";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
-import Loader from "./components/Loader";
-
+import Loader  from "./components/Loader";
 
 
 export default function App() {
